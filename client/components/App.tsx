254fa281d72ef5faa3007539e30770a39ca329bb
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-11-07 15:04:55 +1300

    Adding in App state

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,4 +1,9 @@
+import { useState } from 'react'
+import { Widget } from '../../models/Widget'
+
 function App() {
+  const [widgets, setWidgets] = useState([] as Widget[])
+
   return (
     <div>
       <h1>Widgets for the win!</h1>
